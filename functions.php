e85ef62573590f0bf6f5fb4f8d29e09f6cc3f0df
James Knight <knight.of.design@gmail.com>
2016-02-04 14:42:35 -0500

    Changeth the nav

--- functions.php
+++ functions.php
@@ -11,3 +11,33 @@ function change_excerpt_more($more) {
 	return ' <a class="moretag" href="'. get_permalink($post->ID) . '">Read the full article</a>';
 }
 add_filter('excerpt_more', 'change_excerpt_more');
+
+// Add a Sidebar
+function slick_sidebar_init() {
+  register_sidebar(
+    array(
+      'name'=>__( 'Sidebar', 'slick' ),
+      'id'=>'the_sidebar',
+      'before_widget'=>'<aside id="%1$s" class="widget %2$s">',
+      'after_widget'=>'</aside>','before_title'=>'<h1 class="widget-title">',
+      'after_title'=>'</h1>',
+      )
+    );
+}
+
+add_action( 'widgets_init', 'slick_sidebar_init' );
+function cd_posts_navigation() {
+	?>
+	<nav class="navigation posts-navigation" role="navigation">
+		<h2 class="screen-reader-text"><?php _e( 'Posts navigation', 'codediva' ); ?></h2>
+		<div class="nav-links">
+
+
+			<div class="nav-previous"><?php next_posts_link(__( '&larr; Old posts', 'slick' ) ); ?></div>
+
+
+			<div class="nav-next"><?php previous_posts_link( __( 'New posts &rarr;', 'slick' )  ); ?></div>
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	<?php
+}
---
+++ sidebar.php
@@ -0,0 +1,7 @@
+<div id="secondary" class="sidebar widget-area side-widget-area" role="complementary">
+
+    <div class="sidebar-widgets">
+        <?php dynamic_sidebar( 'the_sidebar' ); ?>
+    </div>
+
+</div><!-- #secondary -->
