c0be948dc6f2a423e013e564451f5165f7440a76
James Knight <knight.of.design@gmail.com>
2016-02-04 08:13:53 -0500

    Customizeth the Excerpts

--- functions.php
+++ functions.php
@@ -3,3 +3,11 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+// Customize Excerpt
+// Replaces the excerpt "more" text by a link
+function change_excerpt_more($more) {
+  global $post;
+	return ' <a class="moretag" href="'. get_permalink($post->ID) . '">Read the full article</a>';
+}
+add_filter('excerpt_more', 'change_excerpt_more');
---
+++ template-parts/content.php
@@ -0,0 +1,28 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php cd_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_excerpt();
+		?>
+
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'codediva' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php cd_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
